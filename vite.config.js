ce5311cb3afdea968545bd33b0255601eebc6ac8
Pawan B <bpawan2002@gmail.com>
2024-06-03 18:45:30 +0530

    vercel config added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@mui/material', '@emotion/react', '@emotion/styled'], // Force Vite to pre-bundle these
+    include: ['@mui/material', '@emotion/react', '@emotion/styled', '@mui/icons-material']
   },
 });
