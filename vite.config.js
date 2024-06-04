146cb0bd6e3862410966e3cdc82d8de99cf45612
Pawan B <bpawan2002@gmail.com>
2024-06-04 09:37:26 +0530

    Deployment Error fixes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/material', '@emotion/react', '@emotion/styled', '@mui/icons-material']
+  },
+  resolve: {
+    alias: {
+      '@mui/icons-material': path.resolve(__dirname, 'node_modules/@mui/icons-material'),
+    },
   },
 });
